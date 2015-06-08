1ac3ebee511e8872497d49d7c5c6af51734b8bf8
Naios <naios-dev@live.de>
2015-06-08 23:43:11 +0200

    some thoughts about callbacks

--- test.cpp
+++ test.cpp
@@ -1,16 +1,37 @@
 
 #include "fluent++.hpp"
 
+template <typename... Args>
+using Callback = std::function<void(Args...)>;
+
+void CastSpell(int id, Callback<bool> const& callback)
+{
+    std::cout << "Cast " << id << std::endl;
+
+    // on success call true
+    callback(true);
+}
+
+void MoveTo(int point, Callback<bool> const& callback)
+{
+    std::cout << "Move to point " << point << std::endl;
+
+    // on success call true
+    callback(true);
+}
+
 int main(int argc, char** argv)
 {
-    make_waterfall()
-        .then([]
+    make_waterfall<Callback<bool>>()
+        .then(std::bind(&CastSpell, 71382, std::placeholders::_1))
+        .then([](bool success, Callback<bool> const& callback)
         {
-            
+            MoveTo(1, callback);
         })
-        .then([]
+        .then([](bool success)
         {
-            
+            // Do something final
+            std::cout << "finish everything" << std::endl;
         });
 
     return 0;
