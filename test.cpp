9602e5fa2668cd6bc4f32efcc0491e315cb77e4b
Denis Blank <denis.blank@outlook.com>
2015-06-08 22:44:14 +0200

    more tests

--- test.cpp
+++ test.cpp
@@ -6,7 +6,11 @@ int main(int argc, char** argv)
     make_waterfall()
         .then([]
         {
-
+            
+        })
+        .then([]
+        {
+            
         });
 
     return 0;
