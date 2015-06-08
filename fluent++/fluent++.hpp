517c9d14f9803d114808cd613b4bec20826eabb0
Naios <naios-dev@live.de>
2015-06-08 22:44:14 +0200

    more tests

--- fluent++/fluent++.hpp
+++ fluent++/fluent++.hpp
@@ -2,10 +2,42 @@
 #ifndef _FLUENT_HPP_
 #define _FLUENT_HPP_
 
+#include <memory>
+#include <utility>
+
 #include "functional_unwrap.hpp"
 
-struct fluent_step
+class fluent_step
 {
+    bool released;
+
+    void release()
+    {
+        int i = 0;
+    }
+
+public:
+    fluent_step() : released(false) { }
+
+    ~fluent_step()
+    {
+        if (!released)
+            release();
+    }
+
+    fluent_step(fluent_step const&) = delete;
+    fluent_step(fluent_step&& right) : released(false)
+    {
+        right.released = true;
+    }
+
+    fluent_step& operator= (fluent_step const&) = delete;
+    fluent_step& operator= (fluent_step&& right)
+    {
+        released = false;
+        right.released = true;
+    }
+
     template <typename Callback>
     fluent_step& then(Callback const& callback)
     {
