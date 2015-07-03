c5c875a5f5c354009229df2ac020555d52cf3bf9
Naios <naios-dev@live.de>
2015-07-03 19:28:11 +0200

    enable non copyable test

--- test.cpp
+++ test.cpp
@@ -376,15 +376,15 @@ int main(int /*argc*/, char** /*argv*/)
                 return CastSpellPromise(20);
             },
             [] {
-                return make_continuable([](Callback<bool, bool, double /*, std::unique_ptr<std::string>*/>&& callback)
+                return make_continuable([](Callback<bool, bool, double , std::unique_ptr<std::string>>&& callback)
                 {
-                    callback(true, false, 0.3f/*, std::unique_ptr<std::string>(new std::string("oh, all work is done!"))*/);
+                    callback(true, false, 0.3f, std::unique_ptr<std::string>(new std::string("oh, all work is done!")));
                 });
             },
             TrivialPromise())
-        .then([](SpellCastResult r0, SpellCastResult r1, bool r2, bool r3, double r4/*, std::unique_ptr<std::string> message*/)
+        .then([](SpellCastResult r0, SpellCastResult r1, bool r2, bool r3, double r4, std::unique_ptr<std::string> message)
         {
-            return TrivialPromise("Lets see... ")/*.then(Log(*message))*/;
+            return TrivialPromise("Lets see... ").then(Log(*message));
         })
         .then([]
         {
