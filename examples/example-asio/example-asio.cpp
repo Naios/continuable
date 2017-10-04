c75baaec901e81a0e5d3d0082e01641a66aac901
Denis Blank <denis.blank@outlook.com>
2017-10-04 22:23:37 +0200

    Add a small asio example

--- examples/example-asio/example-asio.cpp
+++ examples/example-asio/example-asio.cpp
@@ -28,7 +28,11 @@
   SOFTWARE.
 **/
 
+#include <array>
 #include <chrono>
+#include <iostream>
+#include <string>
+#include <system_error>
 
 #include <asio.hpp>
 
@@ -37,34 +41,67 @@
 using namespace std::chrono_literals;
 
 struct functional_io_service : asio::io_service {
+  asio::ip::udp::resolver resolver_;
+
+  functional_io_service() : resolver_(*this) {
+  }
+
   auto post() const noexcept {
     return [this](auto&& work) {
       asio::io_service::post(std::forward<decltype(work)>(work));
     };
   }
+
+  auto wait_async(std::chrono::milliseconds time) {
+    return cti::make_continuable<void>([](auto&& promise) {
+      // ...
+      promise.set_value();
+    });
+  }
+
+  template <typename Protocol>
+  auto async_resolve(Protocol&& protocol, std::string host,
+                     std::string service) {
+    using asio::ip::udp;
+
+    return cti::make_continuable<udp::resolver::iterator>([
+      this, protocol = std::forward<Protocol>(protocol), host = std::move(host),
+      service = std::move(service)
+    ](auto&& promise) mutable {
+      resolver_.async_resolve(
+          host, service,
+          [promise = std::forward<decltype(promise)>(promise)](
+              std::error_code const& error,
+              udp::resolver::iterator iterator) mutable {
+
+            if (error) {
+              promise.set_exception(
+                  std::error_condition(error.value(), error.category()));
+            } else {
+              promise.set_value(std::move(iterator));
+            }
+          });
+    });
+  }
 };
 
-static functional_io_service service;
-
-auto wait_async(std::chrono::milliseconds time) {
-  return cti::make_continuable<void>([](auto&& promise) {
-    // ...
-    promise.set_value();
-  });
-}
-
 int main(int, char**) {
-  wait_async(10s)
-      .then([] {
-        // ...
+  using asio::ip::udp;
 
-        return wait_async(2s);
+  functional_io_service service;
+
+  service.async_resolve(udp::v4(), "127.0.0.1", "daytime")
+      .then([](udp::resolver::iterator iterator) {
+        // ...
+        return *iterator;
       })
-      .then([] {
+      .then([](udp::endpoint endpoint) {
+        // auto socket = std::make_shared<udp::socket>(service);
+        // socket->async_send_to()
+      })
+      .fail([](cti::error_type error) {
         // ...
       });
-
-      asio::tim
 
   service.run();
   return 0;
