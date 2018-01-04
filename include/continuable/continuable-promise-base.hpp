3626133f8ecd9a87f927001ea0c92d0d21858c9c
Denis Blank <denis.blank@outlook.com>
2018-01-04 01:45:09 +0100

    Happy new Year! * Update license notices to 2018

--- include/continuable/continuable-promise-base.hpp
+++ include/continuable/continuable-promise-base.hpp
@@ -7,7 +7,7 @@
                     https://github.com/Naios/continuable
                                    v2.0.0
 
-  Copyright(c) 2015 - 2017 Denis Blank <denis.blank at outlook dot com>
+  Copyright(c) 2015 - 2018 Denis Blank <denis.blank at outlook dot com>
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files(the "Software"), to deal
