a3a9695174999c52ef882af489887787eb232e4e
Denis Blank <denis.blank@outlook.com>
2019-01-04 13:35:44 +0100

    Happy new year!

--- cmake/CMakeLists.txt
+++ cmake/CMakeLists.txt
@@ -1,5 +1,5 @@
 
-# Copyright(c) 2015 - 2018 Denis Blank <denis.blank at outlook dot com>
+# Copyright(c) 2015 - 2019 Denis Blank <denis.blank at outlook dot com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files(the "Software"), to deal
--- include/continuable/continuable-coroutine.hpp
+++ include/continuable/continuable-coroutine.hpp
@@ -6,7 +6,7 @@
                     https://github.com/Naios/continuable
                                    v4.0.0
 
-  Copyright(c) 2015 - 2018 Denis Blank <denis.blank at outlook dot com>
+  Copyright(c) 2015 - 2019 Denis Blank <denis.blank at outlook dot com>
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files(the "Software"), to deal
