8e63a45840b629d77a4aa973f247cdb851d3c1be
Denis Blank <denis.blank@outlook.com>
2020-04-09 15:13:36 +0200

    Increase the version to 4.1.0

--- include/continuable/continuable-base.hpp
+++ include/continuable/continuable-base.hpp
@@ -5,7 +5,7 @@
                         \_,(_)| | | || ||_|(_||_)|(/_
 
                     https://github.com/Naios/continuable
-                                   v4.0.0
+                                   v4.1.0
 
   Copyright(c) 2015 - 2020 Denis Blank <denis.blank at outlook dot com>
 
